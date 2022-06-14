08e9dc641b1a5e3529462421d8638d886a0b56b8
Astroneural <105980825+Astroneural@users.noreply.github.com>
2022-06-14 10:15:58 -0500

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Shaum';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme -- strange \'cause I don\'t ';
+    : 'Next.js Blog Theme -- strange \'cause I don\'t know JS';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
